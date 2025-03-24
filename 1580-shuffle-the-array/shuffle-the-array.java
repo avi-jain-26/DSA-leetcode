56e2d141bba50b3ff609243af20ee1bcdd8dffed
Avi Jain <avijj1726@gmail.com>
2025-03-24 22:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (76.48%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,10 +1,12 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
        int[] arr = new int[(2*n)];
-
+       int ind=0;
        for(int i=0; i<n; i++){
-           arr[i*2] = nums[i];
-           arr[2*i+1]= nums[n+i];
+           arr[ind] = nums[i];
+           ind++;
+           arr[ind]= nums[n+i];
+           ind++;
        } 
 
        return arr;
