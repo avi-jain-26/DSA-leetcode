20aafdd8fed9bb9cd618b70dd83d40ff950a436c
Avi Jain <avijj1726@gmail.com>
2025-03-28 10:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (62.73%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.java
+++ 319-bulb-switcher/bulb-switcher.java
@@ -1,5 +1,15 @@
 class Solution {
-    public int bulbSwitch(int n) {
-        return (int)Math.sqrt(n);
+    public int bulbSwitch(int x) {
+
+        double n = x;
+        double y = 1;
+        double epsilon = 0.000001;
+
+        while (Math.abs(n - y) > epsilon) {
+            n = (n + y) / 2; 
+            y = x/n; 
+        }
+
+        return (int)n;
     }
 }
