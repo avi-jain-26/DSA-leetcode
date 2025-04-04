d11149bb8ad07220be439fd5c518860140ed158e
Avi Jain <avijj1726@gmail.com>
2025-04-04 17:19:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (90.20%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,15 +1,27 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
 
-       if(s.length()!=goal.length()){
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == goal.charAt(0)) {
+
+                StringBuilder sb = new StringBuilder(s);
+            
+                for (int j = 0; j < i; j++) {
+                    sb.append(s.charAt(j));
+                }
+
+                sb.delete(0, i);
+
+                if (sb.toString().equals(goal)) {
+                    return true;
+                }
+            }
+        }
+
         return false;
-       }
-
-       s = s+s;
-
-       if(s.contains(goal)){
-        return true;
-       }
-       return false;
     }
 }
