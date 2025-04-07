2bcceb203e26c9659b12af97979629612084d9fd
Avi Jain <avijj1726@gmail.com>
2025-04-07 11:04:59 +0530

    Time: 4 ms (82.71%) | Memory: 69.2 MB (13.21%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+       
+       ListNode s = head;
+       ListNode f = head;
+
+       //middle
+       while(f !=null && f.next!=null){
+        s = s.next;
+        f = f.next.next;
+       }
+
+       ListNode reversed = reverse(s);
+
+        while(reversed != null){
+
+            if(head.val!=reversed.val){
+                return false;
+            }
+            head= head.next;
+            reversed = reversed.next;
+
+        }
+       return true;
+    }
+
+     public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
