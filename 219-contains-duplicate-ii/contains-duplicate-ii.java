5ba78aea082b323d8dd814fbce80f6bd353c911d
Avi Jain <avijj1726@gmail.com>
2025-04-11 00:44:06 +0530

    Time: 19 ms (60.90%) | Memory: 61 MB (5.22%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -8,7 +8,8 @@ class Solution {
          if(i>k){
             set.remove(nums[i-k-1]);
          }
-
+         //set me element ko add krega or return true;
+         //set me duplicate elment ko add nhi krega and return false;
          if(!set.add(nums[i])){
             return true;
          }
