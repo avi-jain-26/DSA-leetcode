002839acc51394ccf6929acbc8fdadb0fa1642d5
Avi Jain <avijj1726@gmail.com>
2025-09-03 11:02:48 +0530

    Time: 17 ms (12.70%) | Memory: 45.5 MB (13.16%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-        s = s.toLowerCase();
+        s= s.toLowerCase();
         s = s.replaceAll("[^a-z0-9]", "");
         int n = s.length()-1;
-        int i=0;
+        System.out.print(s);
+        int i =0;
 
-        while(i<=n){
-            if(s.charAt(i) != s.charAt(n)){
+        while(i<n){
+            if(s.charAt(i)!=s.charAt(n)){
                 return false;
             }
-            i++;
-            n--;
+           i++;
+           n--;
         }
+        
         return true;
-    }
+   }
 }
